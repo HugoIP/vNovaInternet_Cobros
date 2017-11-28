f1c172f5e443e21218cccab53c3759e37b958a4b
HugoIP <bairon.myh@gmail.com>
2017-11-28 07:00:30 -0600

    Add Logos images css

--- cobros/cfe.php
+++ cobros/cfe.php
@@ -29,7 +29,7 @@ if($_POST["logIn"] != ""){
 
 	<link rel="stylesheet" type="text/css" href="../styles/estilos.css" media="screen">
 	<link rel="stylesheet" type="text/css" href="../styles/cobros.css" media="screen">
-	<link rel="stylesheet" type="text/css" href="../styles/sello.css" media="print">
+	<link rel="stylesheet" type="text/css" href="../styles/sello.css" media="screen">
 	<link rel="stylesheet" type="text/css" href="../styles/clientsPrint.css" media="print">
 	<script type="text/javascript">
     $(document).ready(function() 
