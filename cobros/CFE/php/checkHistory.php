6b3cf295be9367b45ebd58f1e56aa90a3011ebd9
HugoIP <bairon.myh@gmail.com>
2018-08-02 17:33:46 -0500

    Check reponse of php file

--- cobros/CFE/php/checkHistory.php
+++ cobros/CFE/php/checkHistory.php
@@ -2,8 +2,8 @@
 //get data
 $servicio = json_decode($_POST['servicio']);
 //set header
-header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
